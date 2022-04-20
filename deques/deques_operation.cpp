e7d1297c495d957ffff30fc821334561b97f86e9
chaku2204 <86706090+chaku2204@users.noreply.github.com>
2022-04-20 17:46:36 +0530

    Add files via upload

---
+++ deques/deques_operation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+int main()
+{
+    deque<int> dq;
+    dq.push_front(1);
+    dq.push_front(2);
+    dq.push_back(3);
+    
+   
+    for(auto i:dq)
+    {
+        cout<<i<<" ";
+    }
+}
